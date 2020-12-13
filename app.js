51aec256ae678d4eaad039c3f03d72e5e010b877
David <davidacosta25081@yahoo.com>
2020-12-12 23:53:39 -0500

    Event listeners and functions

--- app.js
+++ app.js
@@ -4,3 +4,30 @@ const todoButton = document.querySelector(".todo-button");
 const todoList = document.querySelector(".todo-list");
 
 //Event listeners
+todoButton.addEventListener('click', addTodo);
+
+//Functions
+
+function addTodo(event){
+  event.preventDefault();
+  //Todo DIV
+  const todoDiv = document.createElement("div");
+  todoDiv.classList.add("todo");
+  //create LI
+  const newTodo = document.createElement('li');
+  newTodo.innerText = "Holiii"
+  newTodo.classList.add("todo-item");
+  todoDiv.appendChild(newTodo);
+  //CHECK MARK BUTTON
+  const completedButton = document.createElement("button");
+  completedButton.innerHTML = '<i class="fas fa-check"></i>';
+  completedButton.classList.add("complete-btn");
+  todoDiv.appendChild(completedButton);
+  //CHECK TRASH BUTTON
+  const trashButton = document.createElement("button");
+  trashButton.innerHTML = '<i class="fas fa-trash"></i>';
+  trashButton.classList.add("trash-btn");
+  todoDiv.appendChild(trashButton);
+  //APPEND TO LIST
+  todoList.appendChild(todoDiv);
+}
